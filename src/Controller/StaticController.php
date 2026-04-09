c984b4e141c076b04a300495d84361964eb0a1fd
g-i-kala <5star@wp.pl>
2026-04-09 09:36:39 +0200

    refactor: routes grouping, attributes, filtering, host

--- src/Controller/StaticController.php
+++ src/Controller/StaticController.php
@@ -5,23 +5,47 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+//#[Route('/weatehr')]
 class StaticController extends AbstractController
 {
-    #[Route('weather/highlander-says', name: 'This is highlander', methods: 'GET')]
-    public function highlanderSays(): Response
+    // #[Route('weather/highlander-says/{threshold<\d+>?50}', methods: ['GET', 'POST'], host: 'api.localhost')]
+    public function highlanderSaysApi(int $threshold): Response
     {
         $draw = random_int(0, 100);
-        $forecast = $draw < 50 ? 'Rain' : "Sunny";
+        $forecast = $draw < $threshold ? 'Rain' : "Sunny";
+
+        $json = [
+            'forecast' => $forecast,
+        ];
+        return new JsonResponse($json);
+    }
+
+    // #[Route('weather/highlander-says/{threshold<\d+>?50}', methods: ['GET', 'POST'])]
+    public function highlanderSays(int $threshold): Response
+    {
+        $draw = random_int(0, 100);
+        $forecast = $draw < $threshold ? 'Rain' : "Sunny";
 
         return $this->render('weather/highlander-says.html.twig', [
           'forecast' => $forecast,
         ]);
     }
 
-    #[Route('/', name: 'home route')]
+    // #[Route('weather/highlander-says/{guess}', methods: ['GET', 'POST'])]
+    public function highlanderSaysGuess(string $guess): Response
+    {
+        $forecast = $guess;
+
+        return $this->render('weather/highlander-says.html.twig', [
+          'forecast' => $forecast,
+        ]);
+    }
+
+    // #[Route('/', name: 'home route')]
     public function home(): Response
     {
         $text = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus, perferendis consectetur vero omnis temporibus enim labore distinctio magnam placeat totam explicabo et itaque consequuntur tenetur nihil! Eos alias deserunt possimus.";
